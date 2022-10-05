d232f09be0fa5a18f4124e558149325625256162
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-10-05 09:41:22 +0200

    make the StepperApp generic on the Stepper monad used

--- haskell/lib/MTL/Interpreters/Stepper.hs
+++ haskell/lib/MTL/Interpreters/Stepper.hs
@@ -1,21 +1,20 @@
 module MTL.Interpreters.Stepper (StepperApp (..)) where
 
 import MTL.Core (CoinFlip (flipCoin), Console (printLine))
-import Stepper.Core (askBoundedEnum, notifyStepAndWait)
-import Stepper.Interpreters (IOStepper)
+import Stepper.Core (Stepper, askBoundedEnum, notifyStepAndWait)
 
 -- One can get creative with the possible interpreters, this one for example translates the
--- CoinFlip and Console ability in the IOStepper monad: that is a custom monad that allows
--- to execute the program step-by-step asking the user to input the values it needs to keep
--- the computation going
+-- CoinFlip and Console ability to any monad that can provide the Stepper ability:
+-- it allows to execute the program step-by-step asking the user to input the values it needs
+-- to keep the computation going
 
-newtype StepperApp a = StepperApp {runStepperApp :: IOStepper a}
+newtype StepperApp m a = StepperApp {runStepperApp :: m a}
   deriving (Functor, Applicative, Monad)
 
-instance CoinFlip StepperApp where
-  flipCoin :: StepperApp Bool
+instance (Monad m, Stepper m) => CoinFlip (StepperApp m) where
+  flipCoin :: StepperApp m Bool
   flipCoin = StepperApp $ askBoundedEnum "What is the flip result? "
 
-instance Console StepperApp where
-  printLine :: String -> StepperApp ()
+instance Stepper m => Console (StepperApp m) where
+  printLine :: String -> StepperApp m ()
   printLine = StepperApp . notifyStepAndWait
